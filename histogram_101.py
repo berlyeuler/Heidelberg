14ae696f35c7e74f4ac82292fdfd8a64849f9d36
Beril <datez4660@gmail.com>
2026-07-02 14:34:26 +0200

    Update histogram_101.py

--- histogram_101.py
+++ histogram_101.py
@@ -19,13 +19,20 @@ ds.derived_field_list
 #       üst küme kapsamında yerel alanları da içerebilir).
 
 def hist_function(ds, field, bins=30):
-    ad= ds.all_data()
-    field_data=ad["athena", field] #field_data benim ad'dan çektiğim field değerleri
+    ad = ds.all_data()
+    field_data = ad["athena", field] 
+    
     plt.hist(field_data, bins=bins)
     plt.xlabel(field)
     plt.ylabel('frequency')
     plt.title(f'{field} Histogram')
-    plt.show()
+    
+    # plt.show() yerine bunu yazıyoruz:
+    output_path = '/scratch/hpc-prf-radmix/hpcbeoe/Mach4_test/id0/density_histogram_0040.png'
+    plt.savefig(output_path, dpi=300)
+    print(f"Grafik başarıyla kaydedildi: {output_path}")
+
+hist_function(ds, "density", bins=30)
 
 hist_function(ds, "density", bins=30)
 
