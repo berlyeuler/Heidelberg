0d72abd426c8910e3a4aa144ee3e557a9eec0767
Beril <datez4660@gmail.com>
2026-07-02 13:39:47 +0200

    Modify histogram evolution script for new snapshots
    Updated snapshot numbers and output file path for histogram.

--- hist_evoluion101.py
+++ hist_evoluion101.py
@@ -10,13 +10,13 @@ import imageio
 
 
 if __name__ == "__main__":
-    snap_numbers= ["0000", "0010", "0020", "0030"]
+    snap_numbers= ["0000", "0010", "0020", "0030", "0040", "0060", "0080", "0100"]  # Örnek snapshot numaraları, ihtiyaca göre değiştirilebilir
     #boş bi grafik taslağı açalım
     plt.figure(figsize= (10, 6))
     #tüm snapshotları döngüyle tek tek döndürelim
     for snap in snap_numbers:
         # Klasördeki id0 altındaki vtk dosyalarının yolunu dinamik yapıyoruz
-        file_path= f"/scratch/hpc-prf-radmix/hpcbeoe/test_beril/id0/cloud.{snap}.vtk"
+        file_path= f"/scratch/hpc-prf-radmix/hpcbeoe/Mach4_test/id0/cloud.{snap}.vtk"
         print(f"Yükleniyor: {file_path}")
 
         ds_current= yt.load(file_path) # 1. O anki snapshot dosyasını yüklüyoruz
@@ -32,13 +32,13 @@ if __name__ == "__main__":
 
                        )
     # Döngü bitti, şimdi ortak grafiği süslüyoruz
-    plt.title("Density Histogram Evolution (0000 - 0030)")
+    plt.title("Density Histogram Evolution (0000 - 0100)")
     plt.xlabel("Density (log scale)")
     plt.ylabel("Frequency")
     plt.legend()
     
     # Grafiği kaydedeceğimiz yer
-    output_plot = "/scratch/hpc-prf-radmix/hpcbeoe/test_beril/id0/histogram_evrimi.png"
+    output_plot = "/scratch/hpc-prf-radmix/hpcbeoe/Mach4_test/id0/histogram_evolution.png"
     plt.savefig(output_plot, dpi=300)
     print(f"Saved: {output_plot}")
 
